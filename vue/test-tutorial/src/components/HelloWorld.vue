998e1a588cac7b42ad21bc9aeb81c626b5c89d24
6bjs4112 <tsunenikannsya@gmail.com>
2023-10-24 14:59:27 +0900

    10/24 vue기초

---
+++ vue/test-tutorial/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+    <h1>디지몬 친구들</h1>
+    <h3>레츠고 레츠고</h3>
+    <h5>나는 {{ imgNum }} 번째 이미지</h5>
+    <div class="pick">
+      <button @click="minusNum" class="left">◀</button>
+      <img v-bind:src="imgUrl"/>
+      <button @click="plusNum" class="right">▶</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{imgNum:1, imgUrl:`/img/1.png` }
+  },
+  methods:{
+    minusNum(){
+      this.imgNum >1 ? this.imgNum-- : this.imgNum=8;
+      this.imgUrl=`/img/${this.imgNum}.png`;
+    },
+    plusNum(){
+      this.imgNum <8 ? this.imgNum++ : this.imgNum=1;
+      this.imgUrl=`/img/${this.imgNum}.png`;
+    }
+  },
+
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '/src/assets/test2.scss';
+</style>
---
+++ vue/tutorial/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1 >{{ msg }}</h1>
+    <div class="test"> {{ num }}</div>
+    <button @click="imgChange">이미지 변경</button>
+    <img v-bind:src="reUrl"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return { num:10000, reUrl:'/img/01.jpg', urlNum:1}
+  },
+
+  methods:{
+    imgChange(){
+      this.num += 100;
+      console.log(this.num);
+      //export default{ }이 중괄호 안에 있는 값 this로 가져오기 가능
+
+      this.urlNum < 7 ? this.urlNum+=1 : this.urlNum=1;
+      this.reUrl = `/img/0${this.urlNum}.jpg`
+    }
+  },
+
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  mounted(){
+    const ele = document.querySelector('.hello');
+    console.log(ele);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import "/src/assets/css/test.scss";
+.test{
+  border: 5px solid red;
+}
+// .hello{
+//   h1{font-size: 3rem; color:teal;}
+// }
+</style>
